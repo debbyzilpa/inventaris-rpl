4943752ac81e6666169ae242bdbc40e2d5c449d3
debbyzilpa <zilpadebby@gmail.com>
2024-11-25 10:51:25 +0700

    diperbaiki

--- app/Http/Controllers/KerusakanAlatController.php
+++ app/Http/Controllers/KerusakanAlatController.php
@@ -7,20 +7,17 @@ use Illuminate\Http\Request;
 
 class KerusakanAlatController extends Controller
 {
-    // Menampilkan daftar kerusakan alat
     public function index()
     {
         $kerusakanAlat = KerusakanAlat::all();
         return view('kerusakan_alat.index', compact('kerusakanAlat'));
     }
 
-    // Form untuk menambah kerusakan alat
     public function create()
     {
         return view('kerusakan_alat.create');
     }
 
-    // Menyimpan data kerusakan alat ke database
     public function store(Request $request)
     {
         $request->validate([
@@ -33,21 +30,18 @@ class KerusakanAlatController extends Controller
         return redirect()->route('kerusakan_alat.index')->with('success', 'Data kerusakan alat berhasil ditambahkan.');
     }
 
-    // Menampilkan detail kerusakan alat
     public function show($id)
     {
         $kerusakanAlat = KerusakanAlat::find($id);
         return view('kerusakan_alat.show', compact('kerusakanAlat'));
     }
 
-    // Form untuk edit data kerusakan alat
     public function edit($id)
     {
         $kerusakanAlat = KerusakanAlat::find($id);
         return view('kerusakan_alat.edit', compact('kerusakanAlat'));
     }
 
-    // Update data kerusakan alat
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -61,7 +55,6 @@ class KerusakanAlatController extends Controller
         return redirect()->route('kerusakan_alat.index')->with('success', 'Data kerusakan alat berhasil diperbarui.');
     }
 
-    // Menghapus data kerusakan alat
     public function destroy($id)
     {
         $kerusakanAlat = KerusakanAlat::find($id);
